9166872b151887ea06d63ebd7c3057e4171db504
hitalo <hitaloMazza>
2021-10-09 16:17:53 -0300

    feat: theme

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+interface Props {
+    title: string;
+}
+
+export function Dashboard({ title }: Props) {
+  return <View>
+      <Text>{ title }</Text>
+  </View>;
+}
