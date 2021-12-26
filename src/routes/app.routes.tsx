efaf6713a26d4d308da441a87a8a2ece93682d01
hitalo07 <hitalocunhadesousa@gmail.com>
2021-12-26 16:10:54 -0300

    feat: Resume cards

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { MaterialIcons } from '@expo/vector-icons'
-import { Dashboard } from '../screens/Dashboard'
-import { Register } from '../screens/Register'
 import { useTheme } from 'styled-components'
 import { Platform } from 'react-native'
+
+import { Dashboard } from '../screens/Dashboard'
+import { Register } from '../screens/Register'
+import { Resume } from '../screens/Resume'
 
 const { Navigator, Screen } = createBottomTabNavigator()
 
@@ -52,7 +54,7 @@ export function AppRoutes() {
       />
       <Screen
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: (({ size, color }) => 
             <MaterialIcons
